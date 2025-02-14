d1eba499c5c082c69ff2ae4e8ac2efc07aceea22
ATX8T <76843733+ATX8T@users.noreply.github.com>
2025-02-14 17:48:11 +0800

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Timers;
+using System.Windows.Forms;
 
 namespace FocusAlarmClock
 {
@@ -16,6 +10,8 @@ namespace FocusAlarmClock
         private NotifyIcon notifyIcon;
         private System.Timers.Timer focusTimer;
         private System.Timers.Timer breakTimer;
+        private DateTime focusEndTime;
+        private DateTime breakEndTime;
 
         public Form1()
         {
@@ -34,10 +30,10 @@ namespace FocusAlarmClock
 
         private void InitializeTimers()
         {
-            focusTimer = new System.Timers.Timer();
+            focusTimer = new System.Timers.Timer(1000); // 每秒触发一次
             focusTimer.Elapsed += FocusTimer_Elapsed;
 
-            breakTimer = new System.Timers.Timer();
+            breakTimer = new System.Timers.Timer(1000); // 每秒触发一次
             breakTimer.Elapsed += BreakTimer_Elapsed;
         }
 
@@ -49,20 +45,51 @@ namespace FocusAlarmClock
 
         private void FocusTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            focusTimer.Stop();
-            MessageBox.Show("专注时间结束！");
+            TimeSpan remainingTime = focusEndTime - DateTime.Now;
+            if (remainingTime.TotalSeconds <= 0)
+            {
+                focusTimer.Stop();
+                this.Invoke(new Action(() =>
+                {
+                    this.Show();
+                    this.WindowState = FormWindowState.Normal;
+                    this.BringToFront();
+                    MessageBox.Show("专注时间结束！");
+                }));
+                label8.Invoke(new Action(() => label8.Text = "剩余专注时间：0秒"));
+            }
+            else
+            {
+                label8.Invoke(new Action(() => label8.Text = $"剩余专注时间：{remainingTime.Hours}小时 {remainingTime.Minutes}分钟 {remainingTime.Seconds}秒"));
+            }
         }
 
         private void BreakTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            breakTimer.Stop();
-            MessageBox.Show("休息时间结束！");
+            TimeSpan remainingTime = breakEndTime - DateTime.Now;
+            if (remainingTime.TotalSeconds <= 0)
+            {
+                breakTimer.Stop();
+                this.Invoke(new Action(() =>
+                {
+                    this.Show();
+                    this.WindowState = FormWindowState.Normal;
+                    this.BringToFront();
+                    MessageBox.Show("休息时间结束！");
+                }));
+                label9.Invoke(new Action(() => label9.Text = "剩余休息时间：0秒"));
+            }
+            else
+            {
+                label9.Invoke(new Action(() => label9.Text = $"剩余休息时间：{remainingTime.Hours}小时 {remainingTime.Minutes}分钟 {remainingTime.Seconds}秒"));
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             int hours = 0;
             int minutes = 25;
+            int seconds = 0;
 
             if (int.TryParse(textBox1.Text, out int h))
             {
@@ -74,7 +101,12 @@ namespace FocusAlarmClock
                 minutes = m;
             }
 
-            focusTimer.Interval = (hours * 60 + minutes) * 60 * 1000;
+            if (int.TryParse(textBox5.Text, out int s))
+            {
+                seconds = s;
+            }
+
+            focusEndTime = DateTime.Now.AddHours(hours).AddMinutes(minutes).AddSeconds(seconds);
             focusTimer.Start();
 
             label5.Text = "当前状态：专注中";
@@ -85,6 +117,7 @@ namespace FocusAlarmClock
         {
             int hours = 0;
             int minutes = 3;
+            int seconds = 0;
 
             if (int.TryParse(textBox3.Text, out int h))
             {
@@ -96,7 +129,12 @@ namespace FocusAlarmClock
                 minutes = m;
             }
 
-            breakTimer.Interval = (hours * 60 + minutes) * 60 * 1000;
+            if (int.TryParse(textBox6.Text, out int s))
+            {
+                seconds = s;
+            }
+
+            breakEndTime = DateTime.Now.AddHours(hours).AddMinutes(minutes).AddSeconds(seconds);
             breakTimer.Start();
 
             label5.Text = "当前状态：休息中";
@@ -108,11 +146,15 @@ namespace FocusAlarmClock
             focusTimer.Stop();
             breakTimer.Stop();
             label5.Text = "当前状态：空闲";
+            label8.Text = "剩余专注时间：0秒";
+            label9.Text = "剩余休息时间：0秒";
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             label5.Text = "当前状态：空闲";
+            label8.Text = "剩余专注时间：0秒";
+            label9.Text = "剩余休息时间：0秒";
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -136,6 +178,11 @@ namespace FocusAlarmClock
         }
 
         private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
         {
 
         }
